77f57d44040ed9e4b9a1b80699af27eeeef59837
DesperadoSP <khazix595@yahoo.com>
2017-01-15 19:50:28 -0500

    Made an algorithm to check for potential unwinnable options

--- Customization.java
+++ Customization.java
@@ -24,7 +24,7 @@ public class Customization extends JFrame implements ActionListener{
 	winCount = new JTextField(12);
         
 	Initializer.addActionListener(this);
-	Initializer.setActionCommand("567");
+	Initializer.setActionCommand("Initialize");
 	pane.add(rowLabel);
 	pane.add(rowCount);
 	pane.add(colLabel);
@@ -37,22 +37,48 @@ public class Customization extends JFrame implements ActionListener{
     public void actionPerformed(ActionEvent e){
 	String event = e.getActionCommand();
 	int r, c, w;
-	if(event.equals("567")){
-        
+	Object[] flerp = {"Yes", "No"};
+	if(event.equals("Initialize")){
+	    try{
 	    r = Integer.parseInt(rowCount.getText());
             c = Integer.parseInt(colCount.getText());
-           
-	    derp = new Connect4(r, c);
+	    w = Integer.parseInt(winCount.getText());
+	    if (checkWinnable(r, c, w)){
+            derp = new Connect4(r, c, w);
 	    derp.setVisible(true);
+	    }else {
+		int result = JOptionPane.showOptionDialog(null, "This board is most likely UNWINNABLE, do you still want to start the game with these options?", "Unwinnable", JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, flerp, null);
+		if (result == JOptionPane.YES_OPTION){
+		    derp = new Connect4(r, c, w);
+		    derp.setVisible(true);
+	    }
+	    }
+	    }catch(NumberFormatException j){
+		JOptionPane.showMessageDialog(null, "Please enter valid integers higher than 0.", "Invalid Arguments", JOptionPane.INFORMATION_MESSAGE);
+	    }
 }
     }
+    
+    
 
     
 
 
+    public static boolean checkWinnable(int x, int y, int z){
+	boolean winnable = true;
+	if ((z == 1) || (y == 1)){
+	    winnable = false;
+	    return winnable;
+	}
+	else if((x * y) < (z * z)){
+	    winnable = false;
+	    return winnable;
+       
+	} else{
+	return winnable;
+	    }
 
-
-
+    }
 
     public static void main(String[]args){
 	Customization d = new Customization();
